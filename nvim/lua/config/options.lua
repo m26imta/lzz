8f8bd5ab80ed685e4486a1f0594cfd53f0506e26
m26 <m26@local>
2024-01-12 17:56:53 +0700

    update options.lua - fix sourcing .vimrc

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,7 +1,20 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.cmd("source " .. vim.fn.stdpath("config") .. "/lua/config/.vimrc")
+
+-- Load vimrc
+local home_vimrc = vim.fn.expand("$HOME/.vimrc")
+local config_vimrc = vim.fn.expand(vim.fn.stdpath("config") .. "/lua/config/.vimrc")
+if vim.fn.has("win32") == 1 then
+  home_vimrc = vim.fn.expand("$HOME/_vimrc")
+end
+if vim.fn.filereadable(home_vimrc) == 1 then
+  vim.cmd("source " .. home_vimrc)
+  print("source " .. home_vimrc)
+elseif vim.fn.filereadable(config_vimrc) == 1 then
+  vim.cmd("source " .. config_vimrc)
+  print("source " .. config_vimrc)
+end
 
 -- Neovide
 if vim.g.neovide then
